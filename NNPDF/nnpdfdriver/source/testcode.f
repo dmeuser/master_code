b7f09d1cf67460b6e0389ed75db1b6679322d67b
Danilo Meuser <danilo.meuser@rwth-aachen.de>
2018-05-18 14:43:38 +0200

    changed to use pdf4lhc sets

--- NNPDF/nnpdfdriver/source/testcode.f
+++ NNPDF/nnpdfdriver/source/testcode.f
@@ -5,7 +5,7 @@
       implicit none
       double precision nnxfx
       
-      call initnnset("PDF4LHC15_nlo_mc"//char(0), 2)
+      call initnnset("PDF4LHC15_nlo_mc_pdfas"//char(0), 101)
       write(6,*) nnxfx(0.1d0,100.0d0,5)
       
       end
